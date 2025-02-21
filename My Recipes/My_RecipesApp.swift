62bec914eb8d50a559582f1e283a0fc5b873a8c8
Elif Oskanbaş <elifoskanbas@gmail.com>
2025-02-21 17:11:07 +0300

    Initial Commit

---
+++ My Recipes/My_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_RecipesApp.swift
+//  My Recipes
+//
+//  Created by elif oskanbaş on 21.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct My_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
