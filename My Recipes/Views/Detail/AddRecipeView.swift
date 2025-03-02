7ab89d7b0e04c36ebb556e255afb728b8734566c
Elif Oskanbaş <elifoskanbas@gmail.com>
2025-03-02 20:07:48 +0300

    app logo added and other updates

---
+++ My Recipes/Views/Detail/AddRecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  AddRecipeView.swift
+//  My Recipes
+//
+//  Created by elif oskanbaş on 28.02.2025.
+//
+
+import SwiftUI
+
+struct AddRecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddRecipeView()
+}
